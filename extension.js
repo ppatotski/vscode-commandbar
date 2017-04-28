bacce2a75d3da0282f8aedf9615d31fca16f6dae
Petr Patotski <petr.patotski@siemens.com>
2017-04-28 16:49:14 -0400

    Update commands on a fly

--- extension.js
+++ extension.js
@@ -1,12 +1,10 @@
-'use strict';
 const vscode = require('vscode');
 const childProcess = require( 'child_process' );
-const kill = require( 'tree-kill' );
+const kill = require('tree-kill');
 const fs = require('fs');
 const path = require('path');
 
 const statusBarItems = {};
-const createNewMessage = 'Settings file had been created. Update wont take effect until restart vscode';
 const exampleJson = `{
 	"commands": [
 		{
@@ -23,7 +21,7 @@ const exampleJson = `{
 
 function activate(context) {
 	try {
-		if(vscode.workspace.rootPath) {
+		if(vscode.workspace && vscode.workspace.rootPath) {
 			const settingsPath = path.join(vscode.workspace.rootPath, '.vscode', 'commandbar.json');
 			const channel = vscode.window.createOutputChannel('Commandbar');
 			const vsSettingsCommand = vscode.commands.registerCommand('extension.commandbar.settings', () => {
@@ -42,7 +40,6 @@ function activate(context) {
 										vscode.window.showTextDocument(doc).then((editor) => {
 											editor.edit(edit => {
 												edit.insert(new vscode.Position(0, 0), exampleJson);
-												vscode.window.showInformationMessage(createNewMessage);
 											});
 										});
 									});
@@ -51,107 +48,118 @@ function activate(context) {
 					}
 				});
 			});
-			fs.stat(settingsPath, (err) => {
-				if(!err) {
-					fs.readFile(settingsPath, (err, buffer) => {
-						if(err) {
-							console.error(err);
-						} else {
-							const settings = JSON.parse(buffer);
-							let commandIndex = 0;
-							settings.commands.forEach( (command) => {
-								commandIndex += 1;
-								const alignment = command.alignment === 'right'? vscode.StatusBarAlignment.Right: vscode.StatusBarAlignment.Left;
-								const statusBarItem = vscode.window.createStatusBarItem(alignment, command.priority);
-								const commandId = `extension.commandbar.command_${commandIndex}`;
-								const inProgress = `${command.text} (in progress)`;
+			context.subscriptions.push(vsSettingsCommand);
+			let commandIndex = 0;
+			const refreshCommands = function refreshCommands() {
+				Object.keys(statusBarItems).forEach((key) => {
+					statusBarItems[key].hide();
+				});
+				fs.stat(settingsPath, (err) => {
+					if(err) {
+						console.error(err);
+					} else {
+						fs.readFile(settingsPath, (err, buffer) => {
+							if(err) {
+								console.error(err);
+							} else {
+								const settings = JSON.parse(buffer);
+								settings.commands.forEach( (command) => {
+									commandIndex += 1;
+									const alignment = command.alignment === 'right'? vscode.StatusBarAlignment.Right: vscode.StatusBarAlignment.Left;
+									const statusBarItem = vscode.window.createStatusBarItem(alignment, command.priority);
+									const commandId = `extension.commandbar.command_${commandIndex}`;
+									const inProgress = `${command.text} (in progress)`;
 
-								statusBarItem.text = command.text;
-								statusBarItem.tooltip = command.tooltip;
-								statusBarItem.color = command.color;
-								statusBarItem.command = commandId;
-								statusBarItem.show();
-								statusBarItems[commandId] = statusBarItem;
-								context.subscriptions.push(statusBarItem);
+									statusBarItem.text = command.text;
+									statusBarItem.tooltip = command.tooltip;
+									statusBarItem.color = command.color;
+									statusBarItem.command = commandId;
+									statusBarItem.show();
+									statusBarItems[commandId] = statusBarItem;
+									context.subscriptions.push(statusBarItem);
 
-								if(command.commandType === 'palette') {
-									statusBarItem.command = command.command;
-								} else {
-									const vsCommand = vscode.commands.registerCommand(commandId, () => {
-										const executeCommand = function executeCommand() {
-											const exec = function exec(commandContent) {
-												const process = childProcess.exec(commandContent, { cwd: vscode.workspace.rootPath }, () => {
-													statusBarItem.text = command.text;
-													statusBarItem.process = undefined;
-												});
-												process.stdout.on('data', data => channel.append(data));
-												process.stderr.on('data', data => channel.append(data));
-												statusBarItem.process = process;
+									if(command.commandType === 'palette') {
+										statusBarItem.command = command.command;
+									} else {
+										const vsCommand = vscode.commands.registerCommand(commandId, () => {
+											const executeCommand = function executeCommand() {
+												const exec = function exec(commandContent) {
+													const process = childProcess.exec(commandContent, { cwd: vscode.workspace.rootPath }, () => {
+														statusBarItem.text = command.text;
+														statusBarItem.process = undefined;
+													});
+													process.stdout.on('data', data => channel.append(data));
+													process.stderr.on('data', data => channel.append(data));
+													statusBarItem.process = process;
+												}
+
+												if(command.commandType === 'script') {
+													exec(`npm run ${command.command}`);
+												} else {
+													exec(command.command);
+												}
 											}
 
-											if(command.commandType === 'script') {
-												exec(`npm run ${command.command}`);
-											} else {
-												exec(command.command);
-											}
-										}
+											if(statusBarItem.process) {
+												if(!command.skipTerminateQuickPick) {
+													const options = ['Terminate', 'Terminate and Execute', 'Execute without terminating already running command', 'Cancel'];
 
-										if(statusBarItem.process) {
-											if(!command.skipTerminateQuickPick) {
-												const options = ['Terminate', 'Terminate and Execute', 'Execute without terminating already running command', 'Cancel'];
-
-												vscode.window.showQuickPick(options)
-													.then((option) => {
-														if(option === options[0]) {
-															kill(statusBarItem.process.pid, 'SIGTERM', () => {
-																channel.appendLine('Terminated!');
-																statusBarItem.process = undefined;
-																statusBarItem.text = command.text;
-															});
-														} else if(option === options[1]) {
-															kill(statusBarItem.process.pid, 'SIGTERM', () => {
-																statusBarItem.text = inProgress;
-																channel.appendLine('Terminated!');
-																channel.show();
+													vscode.window.showQuickPick(options)
+														.then((option) => {
+															if(option === options[0]) {
+																kill(statusBarItem.process.pid, 'SIGTERM', () => {
+																	channel.appendLine('Terminated!');
+																	statusBarItem.process = undefined;
+																	statusBarItem.text = command.text;
+																});
+															} else if(option === options[1]) {
+																kill(statusBarItem.process.pid, 'SIGTERM', () => {
+																	statusBarItem.text = inProgress;
+																	channel.show();
+																	channel.clear();
+																	channel.appendLine(`Execute '${command.text}' command...`);
+																	executeCommand();
+																});
+															} else if(option === options[2]) {
 																channel.clear();
 																channel.appendLine(`Execute '${command.text}' command...`);
+																statusBarItem.process = undefined;
 																executeCommand();
-															});
-														} else if(option === options[2]) {
-															channel.clear();
-															channel.appendLine(`Execute '${command.text}' command...`);
-															statusBarItem.process = undefined;
-															executeCommand();
-															statusBarItem.text = inProgress;
-															channel.show();
-														}
+																statusBarItem.text = inProgress;
+																channel.show();
+															}
+														});
+												} else {
+													kill(statusBarItem.process.pid, 'SIGTERM', () => {
+														channel.appendLine('Terminated!');
+														statusBarItem.process = undefined;
+														statusBarItem.text = command.text;
 													});
+												}
 											} else {
-												kill(statusBarItem.process.pid, 'SIGTERM', () => {
-													channel.appendLine('Terminated!');
-													statusBarItem.process = undefined;
-													statusBarItem.text = command.text;
-												});
+												channel.clear();
+												channel.appendLine(`Execute '${command.text}' command...`);
+												executeCommand();
+												statusBarItem.text = inProgress;
+												channel.show();
 											}
-										} else {
-											channel.clear();
-											channel.appendLine(`Execute '${command.text}' command...`);
-											executeCommand();
-											statusBarItem.text = inProgress;
-											channel.show();
-										}
-									});
-									context.subscriptions.push(vsCommand);
-								}
-							});
+										});
+										context.subscriptions.push(vsCommand);
+									}
+								});
 
-						}
-					});
+							}
+						});
+					}
 
+				});
+			}
+			vscode.workspace.onDidSaveTextDocument((doc) => {
+				if(vscode.languages.match({ pattern: settingsPath }, doc)) {
+					refreshCommands();
 				}
-
 			});
-			context.subscriptions.push(vsSettingsCommand);
+			refreshCommands();
 		}
 	} catch(err) {
 		console.error(err);
